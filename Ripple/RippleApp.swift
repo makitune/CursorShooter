97a23acc12abb2b592a5b675013e71295455150a
makitune <msk.kawano@gmail.com>
2022-12-29 20:35:28 +0900

    Initial Commit

---
+++ Ripple/RippleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RippleApp.swift
+//  Ripple
+//
+//  Created by maki on 2022/12/29.
+//
+
+import SwiftUI
+
+@main
+struct RippleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
